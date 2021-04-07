e76cef9de95313cb3acc37904321d94174f5474c
fibarrah20 <81670198+fibarrah20@users.noreply.github.com>
2021-04-07 10:06:29 -0500

    Add files via upload

---
+++ curso_javascript_/004_OperadoresBasicos/05_/file.js
@@ -0,0 +1,27 @@
+'use strict'
+
+console.clear();
+
+/* OPERACION NUMERO UNO */
+console.log("Operacion Numero 1");
+console.log(2+3-4+5/6-4+2);
+console.log(2+3-4+(5/6)-4+2);
+console.log((2+3)-(4+(5/6))-(4+2));
+console.log(((2+3)-(4+(5/6)))-(4+2));
+
+/* OPERACION NUMERO DOS */
+console.log("Operacion Numero 2");
+console.log(23-45-3/3*3+2+1);
+console.log((23-45)-(((3/(3*3))+2)+1));
+
+/* OPERACION NUMERO TRES */
+console.log("Operacion Numero 3");
+console.log(3+4-5+6-7/5*4+3);
+console.log((3+4)-(5+6)-((7/(5*4))+3));
+console.log(((3+4)-(5+6))-((7/(5*4))+3));
+
+/* OPERACION NUMERO CUATRO */
+console.log("Operacion Numero 4");
+console.log(1-2+3*5*5-4/6+3);
+console.log(1-(2+((3*5)*5))-((4/6)+3));
+console.log((1-(2+((3*5)*5)))-((4/6)+3));
